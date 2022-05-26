ed8256a0ed266ee1ae088ad016f65b6f33dc91ad
Mehmet Sayin <sayinmehmet47@Mehmets-MacBook-Pro.local>
2022-05-26 09:07:37 +0200

    starter structure created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [isWide, setWide] = useState(true);
+  const handleToggle = () => {
+    setWide(!isWide);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav className="topbar hidden-sm-up">Topbar</nav>
+      <div className="container__row">
+        <div
+          className={
+            isWide
+              ? "leftbar container__col hidden-sm-down"
+              : "container_row leftbar-small leftbar"
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <div className="container__row">
+            <div className="container__col-10">LeftSideBar</div>
+            <small
+              style={{ float: "right", color: "red", hover: "cursor" }}
+              onClick={handleToggle}
+            >
+              X
+            </small>
+          </div>
+        </div>
+        <div className="dashboard container__col ">
+          <div className="container__row">
+            <div className="container__col-10 dashboard">Dashboard</div>
+            <div className="container__col-2 rightbar hidden-md-down">
+              RightBar
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
